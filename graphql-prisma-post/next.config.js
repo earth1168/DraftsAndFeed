07acd2ffabef96ef10cdaff9ee3f8c73ca17ab8c
Earth <pimanus1234@gmail.com>
2022-06-30 18:35:04 +0700

    Set studio config

--- graphql-prisma-post/next.config.js
+++ graphql-prisma-post/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/api/graphql",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://studio.apollographql.com",
+          },
+          { key: "Access-Control-Allow-headers", value: "Content-type" },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
