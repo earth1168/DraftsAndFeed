ead61f9e7e3c29e29047c172944940f716b24e52
Earth <pimanus1234@gmail.com>
2022-06-30 18:59:16 +0700

    Implement Next-Auth

--- graphql-prisma-post/pages/_app.tsx
+++ graphql-prisma-post/pages/_app.tsx
@@ -1,19 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { SessionProvider } from "next-auth/react";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: '/api/graphql'
-})
+  uri: "/api/graphql",
+});
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <SessionProvider session={session} refetchInterval={5 * 60}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </ApolloProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
