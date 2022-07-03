7571ab9324fef03ad79a3c73f7549a38b0758a7b
Earth <pimanus1234@gmail.com>
2022-07-03 21:25:00 +0700

    Create Drafts, Post, Create Post pages

---
+++ graphql-prisma-post/pages/create.tsx
@@ -0,0 +1,7 @@
+import CreatePost from "../components/CreatePost";
+
+const Create: React.FC = () => {
+  return <CreatePost />;
+};
+
+export default Create;
---
+++ graphql-prisma-post/pages/drafts.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DraftPosts from "../components/DraftPosts";
+
+const Drafts: React.FC = () => {
+  return <DraftPosts />;
+};
+
+export default Drafts;
