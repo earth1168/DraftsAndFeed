07dd9b944cf97991d95e4f628ccb31e374c2c313
Earth <pimanus1234@gmail.com>
2022-07-03 12:47:12 +0700

    Add Navbar

---
+++ graphql-prisma-post/components/Layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import Navbar from "./Navbar";
+
+type Props = {
+  children: ReactNode;
+};
+const Layout: React.FC<Props> = (props) => (
+  <div>
+    <Navbar />
+    <div>{props.children}</div>
+  </div>
+);
+export default Layout;
--- graphql-prisma-post/pages/_app.tsx
+++ graphql-prisma-post/pages/_app.tsx
@@ -1,7 +1,8 @@
-import "../styles/globals.css";
+import "antd/dist/antd.css";
 import type { AppProps } from "next/app";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { SessionProvider } from "next-auth/react";
+import Layout from "../components/Layout";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -12,7 +13,9 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session} refetchInterval={5 * 60}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </SessionProvider>
     </ApolloProvider>
   );
